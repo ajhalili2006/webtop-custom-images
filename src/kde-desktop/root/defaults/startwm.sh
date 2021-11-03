fb78af8cf168d19577914ba28ce718dc45c9517e
Andrei Jiroh Eugenio Halili <ajhalili2006@gmail.com>
2021-11-03 10:54:35 +0800

    Remove all the stuff and start from scratch.
    Time to go into the bloody hell of writing Dockerfiles.
    
    Signed-off-by: Andrei Jiroh Eugenio Halili <ajhalili2006@gmail.com>

--- src/kde-desktop/root/defaults/startwm.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-/usr/bin/pulseaudio --start
-/usr/bin/startplasma-x11 > /dev/null 2>&1
--- src/rd-frontend/root/defaults/startwm.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-/usr/bin/pulseaudio --start
-/usr/bin/openbox-session > /dev/null 2>&1
